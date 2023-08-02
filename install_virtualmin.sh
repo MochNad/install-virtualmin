3b7081adbf614c568890e34aef0ca1ad832fbaa5
MochNad <mochammadnadiraflimaulana@gmail.com>
2023-08-02 13:08:49 +0700

    run_command

--- install_virtualmin.sh
+++ install_virtualmin.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Function to run commands and wait for completion
+run_command() {
+    "$@"
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error occurred. Exiting."
+        exit $exit_code
+    fi
+}
+
 # Step 1: Download Virtualmin Install Script
 clear
 echo "Step 1: Downloading Virtualmin Install Script..."
@@ -8,20 +18,20 @@ wget https://software.virtualmin.com/gpl/scripts/virtualmin-install.sh
 # Step 2: Run the Install Script and Configure Virtualmin
 clear
 echo "Step 2: Running the Install Script and Configuring Virtualmin..."
-yes | sudo sh virtualmin-install.sh
+run_command yes | sudo sh virtualmin-install.sh
 
 # Step 3: Enable sury/php repository
 clear
 echo "Step 3: Enabling sury/php repository..."
-yes | sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
-yes | sudo curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-yes | sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-debian-php-$(lsb_release -sc).list'
-yes | sudo apt-get update
+run_command yes | sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
+run_command yes | sudo curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+run_command yes | sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-debian-php-$(lsb_release -sc).list'
+run_command yes | sudo apt-get update
 
 # Step 4: Install additional PHP packages
 clear
 echo "Step 4: Installing additional PHP packages..."
-yes | sudo apt-get install php8.1-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,xml,zip}
+run_command sudo apt-get install php8.1-{cgi,cli,fpm,pdo,gd,mbstring,mysqlnd,opcache,xml,zip}
 
 # Step 5: Installation complete
 clear
